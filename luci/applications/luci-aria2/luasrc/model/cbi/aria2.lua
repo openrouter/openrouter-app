cf4f09d327a4f8a85458698a589a3a622ddbed35
openrouter <xzfth16@gmail.com>
2014-03-01 22:07:08 +0800

    201403012206

--- luci/applications/luci-aria2/luasrc/model/cbi/aria2.lua
+++ luci/applications/luci-aria2/luasrc/model/cbi/aria2.lua
@@ -1,134 +1,259 @@
 --[[
-LuCI - Lua Configuration Interface - Aria2 support
+LuCI - Lua Configuration Interface - aria2 support
 
-Copyright 2014 nanpuyue <nanpuyue@gmail.com>
+Script by animefans_xj @ nowvideo.dlinkddns.com (af_xj@yahoo.com.cn)
+Based on luci-app-transmission and luci-app-upnp
 
 Licensed under the Apache License, Version 2.0 (the "License");
 you may not use this file except in compliance with the License.
 You may obtain a copy of the License at
 
 	http://www.apache.org/licenses/LICENSE-2.0
+
+$Id$
 ]]--
 
 require("luci.sys")
 require("luci.util")
-require("luci.model.ipkg")
 
-local webui="yaaw"
-local uci = require "luci.model.uci".cursor()
-local running = (luci.sys.call("pidof aria2c > /dev/null") == 0)
-local webinstalled = luci.model.ipkg.installed(webui) 
-local button = ""
-if running and webinstalled then
-	button = "&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;<input type=\"button\" value=\" " .. translate("Open Web Interface") .. " \" onclick=\"window.open('http://'+window.location.hostname+'/" .. webui .. "')\"/>"
+local running=(luci.sys.call("pidof aria2c > /dev/null") == 0)
+local button=""
+if running then
+	button="&nbsp;&nbsp;&nbsp;&nbsp;<input type=\"button\" value=\" " .. translate("Open Aria2 Web Interface") .. " \" onclick=\"window.open('http://'+window.location.host+'/aria2')\"/>"
 end
 
-m = Map("aria2", translate("Aria2 Settings"), translate("Aria2 is a multi-protocol & multi-source download utility, here you can configure the settings.") .. button)
+m=Map("aria2",translate("Aria2 Downloader"),translate("Use this page, you can download files from HTTP FTP and BitTorrent via Aria2.") .. button)
 
-s=m:section(TypedSection, "aria2", translate("Global settings"))
+s=m:section(TypedSection,"aria2",translate("Global"))
 s.addremove=false
 s.anonymous=true
-enable=s:option(Flag, "enabled", translate("Enabled"))
+enable=s:option(Flag,"enabled",translate("Enabled"))
 enable.rmempty=false
-user=s:option(ListValue, "user", translate("Run daemon as user"))
-local p_user
-for _, p_user in luci.util.vspairs(luci.util.split(luci.sys.exec("cat /etc/passwd | cut -f 1 -d :"))) do
-	user:value(p_user)
+--[[function enable.cfgvalue(self,section)
+	return luci.sys.init.enabled("aria2") and self.enabled or self.disabled
 end
+function enable.write(self,section,value)
+	if value == "1" then
+		luci.sys.call("/etc/init.d/aria2 enable >/dev/null")
+		luci.sys.call("/etc/init.d/aria2 start >/dev/null")
+	else
+		luci.sys.call("/etc/init.d/aria2 stop >/dev/null")
+		luci.sys.call("/etc/init.d/aria2 disable >/dev/null")
+	end
+end]]--
+user=s:option(ListValue,"user",translate("Run daemon as user"))
+local list_user
+for _, list_user in luci.util.vspairs(luci.util.split(luci.sys.exec("cat /etc/passwd | cut -f 1 -d :"))) do
+	user:value(list_user)
+end
+--[[event_poll]]--
+event_poll=s:option(ListValue,"event_poll",translate("Event Poll"),translate("Specify the method for polling events. The possible values are epoll, kqueue, port, poll and select. For each epoll, kqueue, port and poll, it is available if system supports it. epoll is available on recent Linux. kqueue is available on various *BSD systems including Mac OS X. port is available on Open Solaris. The default value may vary depending on the system you use."))
+event_poll:value("epoll", translate("epoll"))
+event_poll:value("kqueue", translate("kqueue"))
+event_poll:value("port", translate("port"))
+event_poll:value("poll", translate("poll"))
+event_poll:value("select", translate("select"))
+event_poll.rmempty=false
+event_poll.placeholder="epoll"
+--[[log Enable]]--
+logEnable = s:option(Flag,"logEnable",translate("Log Enabled"),translate("Enable log"))
+logEnable.rmempty = false
 
-file=m:section(TypedSection, "aria2", translate("Files and Locations"))
-file.anonymous=true
-config_dir=file:option(Value, "config_dir", translate("Config file directory"))
-config_dir.placeholder="/var/etc/aria2"
-dir=file:option(Value, "dir", translate("Default download directory"))
-disk_cache=file:option(Value, "disk_cache", translate("Disk cache"), translate("in bytes, You can append K or M"))
-file_allocation=file:option(ListValue, "file_allocation", translate("Preallocation"), translate("\"falloc\" is not available in all cases"))
-file_allocation:value("none", translate("off"))
-file_allocation:value("prealloc", translate("prealloc"))
-file_allocation:value("trunc", translate("trunc"))
-file_allocation:value("falloc", translate("falloc"))
+--[[log Size]]--
+logSize = s:option(ListValue,"logSize",translate("Log Size"), translate("Bytes"))
+logSize:depends("logEnable",1)
+logSize:value("64000", translate("64KB"))
+logSize:value("128000", translate("128KB"))
+logSize:value("256000", translate("256KB"))
+logSize:value("512000", translate("512KB"))
+logSize.rmempty=false
+logSize.placeholder="256000"
 
-task=m:section(TypedSection, "aria2", translate("Task Settings"))
+--[[Log  level]]--
+logSize = s:option(ListValue,"Loglevel",translate("Log Level"))
+logSize:depends("logEnable",1)
+logSize:value("debug", translate("Debug"))
+logSize:value("info", translate("Info"))
+logSize:value("notice", translate("Notice"))
+logSize:value("warn", translate("Warn"))
+logSize:value("error", translate("Error"))
+logSize.rmempty=false
+logSize.placeholder="notice"
+
+--[[check_integrity]]--
+check_integrity = s:option(Flag,"check_integrity",translate("Check Integrity"),translate("Check file integrity by validating piece hashes or a hash of entire file"))
+check_integrity.rmempty = false
+
+--[[Continue?]]--
+Continue = s:option(Flag,"Continue",translate("Continue"),translate("Continue downloading a partially downloaded file."))
+Continue.rmempty = false
+
+--[[file_allocation : aria2 param : file_allocation ]]--
+file_allocation = s:option(ListValue, "file_allocation", translate("File Allocation"), translate("none: doesn't pre-allocate file space. prealloc:pre-allocates file space before download begins.This may take some time depending on the size ofthe file.If you are using newer file systems such as ext4(with extents support), btrfs, xfs or NTFS(MinGW build only), falloc is your bestchoice. It allocates large(few GiB) filesalmost instantly. Don't use falloc with legacyfile systems such as ext3 and FAT32 because ittakes almost same time as prealloc and itblocks aria2 entirely until allocation finishes.falloc may not be available if your system doesn't have posix_fallocate() function.trunc : uses ftruncate() system call or platform-specific counterpart to truncate a file to a specified length."))
+file_allocation:value("none","none")
+file_allocation:value("prealloc","prealloc")
+file_allocation:value("trunc","trunc")
+file_allocation:value("falloc","falloc")
+file_allocation.rmempty=false
+file_allocation.placeholder="falloc"
+
+         --[[Enable_mmap  ]]--
+Enable_mmap = s:option(Flag,"Enable_mmap",translate("Enable MMap"),translate("Map files into memory"))
+Enable_mmap.rmempty = false
+Enable_mmap:depends("file_allocation", "prealloc")
+Enable_mmap:depends("file_allocation", "trunc")
+Enable_mmap:depends("file_allocation", "falloc")
+
+--[[disk_cache : aria2 param : disk_cache ]]--
+disk_cache = s:option(ListValue, "disk_cache", translate("Disk Cache"), translate("Disk Cache Size"))
+disk_cache:value("1M","1M")
+disk_cache:value("2M","2M")
+disk_cache:value("3M","3M")
+disk_cache.rmempty=false
+disk_cache.placeholder="1M"
+
+--[[min_split_size : aria2 param : min_split_size]]--
+min_split_size = s:option(ListValue, "min_split_size", translate("Min split size"), translate("MB The min size to split the file to pieces to download"))
+min_split_size:value("10M","10M")
+min_split_size:value("20M","20M")
+min_split_size:value("30M","30M")
+min_split_size.rmempty = false
+min_split_size.placeholder = "30M"
+
+location=m:section(TypedSection,"aria2",translate("Location"))
+location.addremove=false
+location.anonymous=true
+config_dir=location:option(Value,"config_dir",translate("Config Directory"))
+config_dir.placeholder="/mnt/sda1/.Programs/aria2"
+download_dir=location:option(Value,"download_dir",translate("Download Directory"))
+download_dir.placeholder="/mnt/sda1/Downloads"
+
+task=m:section(TypedSection,"aria2",translate("Task"))
+task.addremove=false
 task.anonymous=true
-overall_speed_limit=task:option(Flag, "overall_speed_limit", translate("Overall speed limit enabled"))
-max_overall_download_limit=task:option(Value, "max_overall_download_limit", translate("Overall download limit"), translate("in bytes/sec, You can append K or M"))
-max_overall_download_limit:depends("overall_speed_limit", "1")
-max_overall_upload_limit=task:option(Value, "max_overall_upload_limit", translate("Overall upload limit"), translate("in bytes/sec, You can append K or M"))
-max_overall_upload_limit:depends("overall_speed_limit", "1")
-task_speed_limit=task:option(Flag, "task_speed_limit", translate("Per task speed limit enabled"))
-max_download_limit=task:option(Value, "max_download_limit", translate("Per task download limit"), translate("in bytes/sec, You can append K or M"))
-max_download_limit:depends("task_speed_limit", "1")
-max_upload_limit=task:option(Value, "max_upload_limit", translate("Per task upload limit"), translate("in bytes/sec, You can append K or M"))
-max_upload_limit:depends("task_speed_limit", "1")
-max_concurrent_downloads=task:option(Value, "max_concurrent_downloads", translate("Max concurrent downloads"))
-max_concurrent_downloads.placeholder="5"
-max_connection_per_server=task:option(Value, "max_connection_per_server", translate("Max connection per server"), "1-16")
-max_connection_per_server.datetype="range(1, 16)"
-max_connection_per_server.placeholder="1"
-min_split_size=task:option(Value, "min_split_size", translate("Min split size"), "1M-1024M")
-min_split_size.placeholder="20M"
-split=task:option(Value, "split", translate("Max number of split"))
+restore_task=task:option(Flag,"restore_task",translate("Restore unfinished task when boot"))
+restore_task.rmempty=false
+--[[load_cookies]]--
+load_cookies=task:option(Flag,"load_cookies", translate("Load Cookies from FILE using the Firefox3 format and Mozilla/Firefox(1.x/2.x)/Netscape format.(Aria2Cookies.cookies)"))
+load_cookies.rmempty = false
+--[[ save_interval : aria2 param : --auto-save-interval ]]--
+save_interval=task:option(Value,"save_interval",translate("Save interval"),translate("In seconds, 0 means unsave and let tasks can't be restore"))
+save_interval:value("5","5")
+save_interval:value("10","10")
+save_interval:value("20","20")
+save_interval:value("30","30")
+save_interval:value("40","40")
+save_interval:value("50","50")
+save_interval:value("60","60")
+save_interval.rmempty=true
+save_interval.placeholder="60"
+save_interval.datatype="range(0,600)"
+--[[ queue_size : aria2 param : -j ]]--
+queue_size=task:option(Value,"queue_size",translate("Download queue size"))
+queue_size:value("1","1")
+queue_size:value("2","2")
+queue_size:value("3","3")
+queue_size:value("4","4")
+queue_size:value("5","5")
+queue_size.rmempty=true
+queue_size.placeholder="2"
+queue_size.datatype="range(1,20)"
+--[[ split : aria2 param : -s ]]--
+split=task:option(Value,"split",translate("Blocks of per task"))
+split:value("1","1")
+split:value("2","2")
+split:value("3","3")
+split:value("4","4")
+split:value("5","5")
+split:value("6","6")
+split:value("7","7")
+split:value("8","8")
+split:value("9","9")
+split.rmempty=true
 split.placeholder="5"
-save_session_interval=task:option(Value, "save_session_interval", translate("Autosave session interval"), translate("Sec"))
-save_session_interval.default="30"
+split.datatype="range(1,20)"
+--[[ thread : aria2 param : -x ]]--
+thread=task:option(ListValue,"thread",translate("Download threads of per server"))
+thread:value("1","1")
+thread:value("2","2")
+thread:value("3","3")
+thread:value("4","4")
+thread:value("5","5")
+thread:value("6","6")
+thread:value("7","7")
+thread:value("8","8")
+thread:value("9","9")
+thread:value("10","10")
 
-bittorrent=m:section(TypedSection, "aria2", translate("BitTorrent Settings"))
-bittorrent.anonymous=true
-enable_dht=bittorrent:option(Flag, "enable_dht", translate("<abbr title=\"Distributed Hash Table\">DHT</abbr> enabled"))
-enable_dht.enabled="true"
-enable_dht.disabled="false"
-bt_enable_lpd=bittorrent:option(Flag, "bt_enable_lpd", translate("<abbr title=\"Local Peer Discovery\">LPD</abbr> enabled"))
-bt_enable_lpd.enabled="true"
-bt_enable_lpd.disabled="false"
-follow_torrent=bittorrent:option(Flag, "follow_torrent", translate("Follow torrent"))
-follow_torrent.enabled="true"
-follow_torrent.disabled="false"
-listen_port=bittorrent:option(Value, "listen_port", translate("BitTorrent listen port"))
-listen_port.placeholder="6881-6999"
-bt_max_peers=bittorrent:option(Value, "bt_max_peers", translate("Max number of peers per torrent"))
-bt_max_peers.placeholder="55"
-bt_tracker_enable=bittorrent:option(Flag, "bt_tracker_enable", translate("Additional Bt tracker enabled"))
-bt_tracker=bittorrent:option(DynamicList, "bt_tracker", translate("List of additional Bt tracker"))
-bt_tracker:depends("bt_tracker_enable", "1")
-bt_tracker.rmempty=true
+--[[force_save ]]--
+force_save=task:option(Flag, "force_save", translate("Force Save"), translate("Save download with --save-session option even if the download is completed or removed"))
+force_save.rmempty = false
 
-function bt_tracker.cfgvalue(self, section)
-	local rv = { }
 
-	local val = Value.cfgvalue(self, section)
-	if type(val) == "table" then
-		val = table.concat(val, ",")
-	elseif not val then
-		val = ""
-	end
+--[[max_tries]]--
+max_tries=task:option(Value, "max_tries", translate("Max Trie Times"), translate("Max Try Times,0 Means No Limit"))
+max_tries:value("0","NoLimit")
+max_tries:value("10","10Times")
+max_tries:value("20","10Times")
+max_tries.rmempty=false
+max_tries.placeholder="0"
+max_tries.datatype="range(0,1000)"
 
-	local file
-	for file in val:gmatch("[^,%s]+") do
-		rv[#rv+1] = file
-	end
+--[[max_tries]]--
+retry_wait=task:option(Value, "retry_wait", translate("Retry Wait Time"), translate("Retry Wait Time SEC"))
+retry_wait:value("20","20Seconds")
+retry_wait:value("30","30Seconds")
+retry_wait:value("60","1Minute")
+retry_wait.rmempty=false
+retry_wait.placeholder="20"
 
-	return rv
-end
+network=m:section(TypedSection,"aria2",translate("Network"))
+network.addremove=false
+network.anonymous=true
+disable_ipv6=network:option(Flag,"disable_ipv6",translate("Disable IPv6"))
+disable_ipv6.rmempty=false
+enable_lpd=network:option(Flag,"enable_lpd",translate("Enable Local Peer Discovery"))
+enable_lpd.rmempty=false
+enable_dht=network:option(Flag,"enable_dht",translate("Enable DHT Network"))
+enable_dht.rmempty=false
+listen_port=network:option(Value,"listen_port",translate("Port for BitTorrent"))
+listen_port.placeholder="6882"
+listen_port.datatype="range(1,65535)"
+download_speed=network:option(Value,"download_speed",translate("Download speed limit"),translate("In KB/S, 0 means unlimit"))
+download_speed.placeholder="0"
+download_speed.datatype="range(0,100000)"
+upload_speed=network:option(Value,"upload_speed",translate("Upload speed limit"),translate("In KB/S, 0 means unlimit"))
+upload_speed.placeholder="0"
+upload_speed.datatype="range(0,100000)"
 
-function bt_tracker.write(self, section, value)
-	local rv = { }
-	local file
-	for file in luci.util.imatch(value) do
-		rv[#rv+1] = file
-	end
-	Value.write(self, section, table.concat(rv, ","))
-end
-
-rpc=m:section(TypedSection, "aria2", translate("RPC settings"))
+rpc=m:section(TypedSection,"aria2",translate("Remote Control"))
+rpc.addremove=false
 rpc.anonymous=true
-rpc_listen_port=rpc:option(Value, "rpc_listen_port", translate("RPC port"))
-rpc_listen_port.datatype="port"
-rpc_listen_port.placeholder="6800"
-rpc_auth_required=rpc:option(Flag, "rpc_auth_required", translate("RPC authentication required"))
-rpc_user=rpc:option(Value, "rpc_user", translate("RPC username"))
-rpc_user:depends("rpc_auth_required", "1")
-rpc_passwd=rpc:option(Value, "rpc_passwd", translate("RPC password"))
-rpc_passwd:depends("rpc_auth_required", "1")
-rpc_passwd.password = true
+rpc_auth=rpc:option(Flag,"rpc_auth",translate("Use RPC Auth"))
+rpc_auth.rmempty=false
+rpc_user=rpc:option(Value,"rpc_user",translate("User name"))
+rpc_user.placeholder="admin"
+rpc_user:depends("rpc_auth",1)
+rpc_password=rpc:option(Value,"rpc_password",translate("Password"))
+rpc_password.placeholder="admin"
+rpc_password:depends("rpc_auth",1)
+
+advanced=m:section(TypedSection,"aria2",translate("Advanced"))
+advanced.addremove=false
+advanced.anonymous=true
+    --[[user_agent]]
+user_agent=advanced:option(Value, "user_agent", translate("User Agent"), translate("Specify User Agent"))
+user_agent.placeholder = "Transmission/2.72 (13582)"
+
+    --[[peer_id_prefix]]
+peer_id_prefix=advanced:option(Value, "peer_id_prefix", translate("Peer ID Prefix"), translate("Specify The Prefix Of Peer ID"))
+peer_id_prefix.placeholder = "-TR2720-"
+
+extra_cmd=advanced:option(Flag,"extra_cmd",translate("add extra commands"))
+extra_cmd.rmempty=false
+cmd_line=advanced:option(Value,"cmd_line",translate("Command-Lines"),translate("To check all commands availabled, visit:") .. "&nbsp;<a onclick=\"window.open('http://'+window.location.host+'/aria2/help.htm')\" style=\"cursor:pointer\"><font color='blue'><i><u>http://aria2.sourceforge.net/manual/en/html/index.html</u></i></font></a>")
+cmd_line:depends("extra_cmd",1)
 
 return m
+
+
---
+++ luci/applications/luci-nwan/luasrc/controller/nwan.lua
@@ -0,0 +1,21 @@
+module("luci.controller.nwan", package.seeall)
+
+function index()
+
+	if nixio.fs.access("/etc/config/nwan") then
+
+	local page 	
+	page = entry({"admin", "services", "nwan"}, cbi("nwan/nwan"), _("N-WAN"),50)
+	page.i18n = "nwan"
+	page.dependent = true
+	end
+
+	if nixio.fs.access("/etc/config/nwannumset") then
+	local page 
+	page = entry({"admin", "nwannumset"}, cbi("nwan/nwannumset"), _("macvlan"), 59)
+	page.i18n = "nwan"
+	page.dependent = true
+	end
+
+
+end
